7fa75047ab9fe66b9a79eb08d7c29d9519bad22d
Emerson de Baldi <edbemerson@gmail.com>
2022-05-20 20:03:33 -0400

    Conexão com DB
    Arquivo de Conexão com o Banco de Dados

--- connection.php
+++ connection.php
@@ -4,27 +4,27 @@
 	Data: 20/05/2022 18:57:31
 */
 
-define('HOST', 'nome host de conexexao');			// padrão é localhost
-define('USER', 'nome usuario banco de dados');		// padrão é root
-define('PASS', 'senha banco de dados');				// padrão é vazio
-define('DB', 'nome do banco de dados');				// nome do banco de dados que você criou no phpMyAdmin  
+define('HOST', 'localhost');						// nome host de conexão
+define('USER', 'root');								// nome usuário banco de dados
+define('PASS', '');									// padrão é vazio
+define('DB', 'listar');								// nome do banco de dados que você criou no phpMyAdmin  
 
 
 function conexaoDB(){
 	
 	try{
 		$dsn = "mysql:host=" . HOST . "; dbname=" . DB ;
-		echo "Banco de Dados Conectado...";			// se o db conectar vai apresentar esta msg na tela
+		echo "Banco de Dados Conectado...<br>";			// se o db conectar vai apresentar esta msg na tela
 		
 		$con = new PDO($dsn, USER, PASS);
-		echo "Banco de Dados Selecionado...";		// se o db selecionar vai apresentar esta msg na tela
+		echo "Banco de Dados Selecionado...<br>";		// se o db selecionar vai apresentar esta msg na tela
 		
 		$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		
 		return $con;
 	
 	} catch (PDOException $ex){
-		echo "erro -> " . $ex->getMessage();
+		echo "erro -> " . $ex->getMessage();			// se contiver algum erro no nome dos HOST, USER, PASS ou DB vai apresentar uma msg
 		
 	}
 }
